d7630c79aeb4e76b9e89f57be3bc3352279c3670
Samuel Nunes <samuthekid@gmail.com>
2026-05-26 01:10:04 +0100

    Format code

--- Sources/BlockedBySquare/OverlayWindow.swift
+++ Sources/BlockedBySquare/OverlayWindow.swift
@@ -1,41 +1,53 @@
 import AppKit
 
 class OverlayWindow: NSWindow {
-    private var content: OverlayView!
+  private var content: OverlayView!
 
-    init(screen: NSScreen) {
-        super.init(
-            contentRect: screen.frame,
-            styleMask: .borderless,
-            backing: .buffered,
-            defer: false
-        )
+  init(screen: NSScreen) {
+    super.init(
+      contentRect: screen.frame,
+      styleMask: .borderless,
+      backing: .buffered,
+      defer: false
+    )
 
-        self.level = NSWindow.Level(rawValue: Int(CGWindowLevelForKey(.screenSaverWindow)) - 1)
-        self.backgroundColor = .clear
-        self.isOpaque = false
-        self.hasShadow = false
-        self.ignoresMouseEvents = true
-        self.isReleasedWhenClosed = false
-        self.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary, .stationary]
+    self.level = NSWindow.Level(rawValue: Int(CGWindowLevelForKey(.screenSaverWindow)) - 1)
+    self.backgroundColor = .clear
+    self.isOpaque = false
+    self.hasShadow = false
+    self.ignoresMouseEvents = true
+    self.isReleasedWhenClosed = false
+    self.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary, .stationary]
 
-        content = OverlayView(frame: NSRect(origin: .zero, size: screen.frame.size))
-        self.contentView = content
-        self.setFrame(screen.frame, display: false)
-    }
+    content = OverlayView(frame: NSRect(origin: .zero, size: screen.frame.size))
+    self.contentView = content
+    self.setFrame(screen.frame, display: false)
+  }
 
-    override var canBecomeKey: Bool { false }
-    override var canBecomeMain: Bool { false }
+  override var canBecomeKey: Bool { false }
+  override var canBecomeMain: Bool { false }
 
-    func showSquare(at localPos: CGPoint) { content.showSquare(at: localPos) }
-    func hideSquare()                     { content.hideSquare() }
-    func updatePhrases(top: String, bottom: String) { content.updatePhrases(top: top, bottom: bottom) }
-    func updateTextColors(topLight: NSColor, topDark: NSColor, bottomLight: NSColor, bottomDark: NSColor) {
-        content.updateTextColors(topLight: topLight, topDark: topDark,
-                                 bottomLight: bottomLight, bottomDark: bottomDark)
-    }
+  func showSquare(at localPos: CGPoint) {
+    content.showSquare(at: localPos)
+  }
 
-    func updatePadding(top: CGFloat, bottom: CGFloat) {
-        content.updatePadding(top: top, bottom: bottom)
-    }
+  func hideSquare() {
+    content.hideSquare()
+  }
+
+  func updatePhrases(top: String, bottom: String) {
+    content.updatePhrases(top: top, bottom: bottom)
+  }
+
+  func updateTextColors(
+    topLight: NSColor, topDark: NSColor, bottomLight: NSColor, bottomDark: NSColor
+  ) {
+    content.updateTextColors(
+      topLight: topLight, topDark: topDark,
+      bottomLight: bottomLight, bottomDark: bottomDark)
+  }
+
+  func updatePadding(top: CGFloat, bottom: CGFloat) {
+    content.updatePadding(top: top, bottom: bottom)
+  }
 }
